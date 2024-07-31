b2918e8333d8d0ff23f5097e07be1e84dbfec9f6
kleidis <167202775+kleidis@users.noreply.github.com>
2024-07-31 16:54:09 +0200

    scripts: fix autostart script

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -15,5 +15,12 @@ if ! pgrep -x "syncthingtray-qt6" > /dev/null; then
     syncthingtray-qt6 &> /dev/null &
 fi
 
+# Check if CopyQ is already running, if not, start it quietly in the background
+if ! pgrep -x "copyq" > /dev/null; then
+    flatpak run com.github.hluk.copyq &> /dev/null &
+fi
 
-
+# Check if kde-connectd is already running, if not, start it quietly in the background
+if ! pgrep -x "kdeconnect-indicator" > /dev/null; then
+    kdeconnect-indicator &> /dev/null &
+fi
